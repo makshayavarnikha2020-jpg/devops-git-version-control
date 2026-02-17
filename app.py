5b5e8251ccc3e8ea8bffbf93e91aa914c472d5bc
makshayavarnikha2020-jpg <makshayavarnikha2020@gmail.com>
2026-02-17 22:27:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+"""
+DevOps Git Version Control Project
+Author: Sai
+"""
+
+def main():
+    print("DevOps Version-Controlled Project")
+    print("Using Git Best Practices")
+
+if __name__ == "__main__":
+    main()
